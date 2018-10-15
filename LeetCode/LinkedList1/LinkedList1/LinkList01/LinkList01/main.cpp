3d24e65b81e84e524384314ed2548103080ae2d7
zx1314621 <935638374@qq.com>
2018-10-15 14:54:42 +0800

    First Commit Test

---
+++ LeetCode/LinkedList1/LinkedList1/LinkList01/LinkList01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkList01
+//
+//  Created by Xiangyu Liu on 2018/10/15.
+//  Copyright © 2018 Xiangyu Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
